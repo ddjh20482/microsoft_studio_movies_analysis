e79402919689a423de6e015418fd4151b6ea205c
Jinhoon <ddjh204@gmail.com>
2022-03-23 11:06:58 -0400

    update visualizations

--- code/visualizations.py
+++ code/visualizations.py
@@ -31,21 +31,29 @@ def profit(df):
     
     pass
 
-def budget(df):
+def budget(budget):
 
-    fig, ax = plt.subplots(figsize=(8,6), facecolor=('#d5d9d7'))
+    fig, ax = plt.subplots(figsize=(10,6), facecolor=('#d5d9d7'))
 
-    x = df.budget.loc[df.budget > 0] / 1000000
+    x = budget.Median
+    y = budget.Q25
+    z = budget.Q75
+    index = budget.index.values
 
     ax.set_facecolor('#f0ffff')
 
-    ax.hist(x, bins=17, color = '#0343df')
-    ax.set_title('Budget Distribution', fontsize=30)
-    ax.set_xlabel('Budget in Million Dollar', fontsize=20)
-    ax.set_ylabel('Count', fontsize=20)
+    ax.bar(index, x, color = '#0000ff')
+    ax.plot(index, y, color = '#00ff00', linewidth=4.00)
+    ax.plot(index, z, color = '#f97306', linewidth=4.00)
+    ax.legend(['Q25', 'Q75', 'median'])
+    ax.yaxis.set_major_formatter('${x:1.0f}M')
+    ax.set_title('Budget by Profit Range', fontsize=30)
+    ax.set_xlabel('Profit Range', fontsize=20)
+    ax.set_ylabel('Budget', fontsize=20)
     ax.tick_params(labelcolor='#0000ff')
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
+    ax.tick_params(axis='x', labelrotation = 15)
     
     plt.show() 
     
@@ -55,14 +63,21 @@ def genre(Genre):
 
     fig, ax = plt.subplots(figsize=(10,8), facecolor=('#d5d9d7'))
 
-    y = Genre.Genre_name
-    x = Genre.Genre_count
+    x = Genre.Median
+    y = Genre.Q25
+    z = Genre.Q75
+    label = Genre.Genre_name
 
-    ax.set_facecolor('#eafff5')
-
-    ax.barh(y, x, height=0.7, color = 'g')
-    ax.set_title('Genre Popularity', fontsize=30)
-    ax.set_xlabel('Count', fontsize=20)
+    ax.set_facecolor('#f0ffff')
+    
+    ax.barh(label, x, height=0.7, color = '#0000ff')
+    ax.plot(y, label, color = '#00ff00', linewidth=4.00)
+    ax.plot(z, label, color = '#f97306', linewidth=4.00)
+    ax.legend(['Q25', 'Q75', 'median'])
+    ax.xaxis.set_major_formatter('${x:1.0f}M')
+    
+    ax.set_title('Profit by Genre', fontsize=30)
+    ax.set_xlabel('Profit', fontsize=20)
     ax.set_ylabel('Genre Name', fontsize=20)
     ax.tick_params(labelcolor='#0000ff')
     plt.xticks(fontsize=15)
@@ -72,18 +87,25 @@ def genre(Genre):
 
     pass
 
-def runtime(df):
+def runtime(runtime):
 
     fig, ax = plt.subplots(figsize=(8,6), facecolor=('#d5d9d7'))
 
-    x = df.runtime_minutes
+    x = runtime.Median
+    y = runtime.Q25
+    z = runtime.Q75
+    index = runtime.index.values
 
-    ax.set_facecolor('#eafff5')
+    ax.set_facecolor('#f0ffff')
 
-    ax.hist(x, bins=21, color = 'c')
-    ax.set_title('Movie Runtime Distribution', fontsize=30)
-    ax.set_xlabel('Movie Length in Minutes', fontsize=20)
-    ax.set_ylabel('Count', fontsize=20)
+    ax.bar(index, x, color = '#0000ff')
+    ax.plot(index, y, color = '#00ff00', linewidth=4.00)
+    ax.plot(index, z, color = '#f97306', linewidth=4.00)
+    ax.legend(['Q25', 'Q75', 'median'])
+    ax.yaxis.set_major_formatter('${x:1.0f}M')
+    ax.set_title('Profit by Runtime', fontsize=30)
+    ax.set_xlabel('Runtime Ranges in Minutes', fontsize=20)
+    ax.set_ylabel('Profit', fontsize=20)
     ax.tick_params(labelcolor='#0000ff')
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
